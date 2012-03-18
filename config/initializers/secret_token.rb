1646278c5415b2be993b9a4955f7b1248a73106c
Carlos Emanuel Mathiasen <matt987@mathiasen.com>
2012-03-18 06:39:23 -0300

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YieldMatt::Application.config.secret_token = 'be53296fb8b92ca883dc4efe0e1f7eba82b77dc7b6c34025cd3128f5d5b8cc9774693db29ba9e7113e048bab60998aa7b47e545add94ddd2214908fe2c178594'
